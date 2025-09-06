721621d2ea3e24dd716ce77af2cddfad3be32329
beatlinemedia <beatlinemedia@gmail.com>
2025-09-06 03:01:38 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Artists from './components/Artists';
+import Releases from './components/Releases';
+import Contact from './components/Contact';
+import Footer from './components/Footer'; // footer import
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Artists />
+        <Releases />
+        <Contact />
+      </main>
+      
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
